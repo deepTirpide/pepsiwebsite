9c1f24f54670a8426499932aa190c25faa8e6f6d
deep tirpude <128034228+deepTirpide@users.noreply.github.com>
2023-04-01 11:28:48 +0530

    Add files via upload

---
+++ pepsiweb.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+import logo from '@salesforce/resourceUrl/pp';
+import pepsiImg from '@salesforce/resourceUrl/pepsi';
+
+export default class pepsiweb extends LightningElement {
+  logo = logo;
+  pepsiImg = pepsiImg;
+
+  handleLogin() {
+    // Logic for handling login button click
+  }
+
+  handleSignup() {
+    // Logic for handling signup button click
+  }
+
+  handleBuy() {
+    // Logic for handling buy button click
+  }
+}
